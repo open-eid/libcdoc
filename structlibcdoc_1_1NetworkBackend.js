4b782e56f52bc5c9f0a62885ec5f4d854b110fc0
lauris71 <lauris71@users.noreply.github.com>
2025-03-27 12:01:17 +0000

    deploy: f239344c83dbc1bf63b58b3af6a407757f5da08c

---
+++ structlibcdoc_1_1NetworkBackend.js
@@ -0,0 +1,27 @@
+var structlibcdoc_1_1NetworkBackend =
+[
+    [ "CapsuleInfo", "structlibcdoc_1_1NetworkBackend_1_1CapsuleInfo.html", "structlibcdoc_1_1NetworkBackend_1_1CapsuleInfo" ],
+    [ "ShareInfo", "structlibcdoc_1_1NetworkBackend_1_1ShareInfo.html", "structlibcdoc_1_1NetworkBackend_1_1ShareInfo" ],
+    [ "NetworkBackend", "structlibcdoc_1_1NetworkBackend.html#a2d5199c3336e81dc1bacae9113892727", null ],
+    [ "~NetworkBackend", "structlibcdoc_1_1NetworkBackend.html#ada3088d2bbccfb857d3471491543e023", null ],
+    [ "NetworkBackend", "structlibcdoc_1_1NetworkBackend.html#a905d38a04f8cfe534e8e90537bfe6a34", null ],
+    [ "CDOC_DISABLE_MOVE", "structlibcdoc_1_1NetworkBackend.html#a4313d1c47c5d973beea338cbbaefeb41", null ],
+    [ "fetchKey", "structlibcdoc_1_1NetworkBackend.html#a2939e61cdff08768177ecfb43cd9f900", null ],
+    [ "fetchNonce", "structlibcdoc_1_1NetworkBackend.html#a9b04db35c9784e19e86effffcf1b009d", null ],
+    [ "fetchShare", "structlibcdoc_1_1NetworkBackend.html#afdcf0caea85df1074a75baa0627d2c6e", null ],
+    [ "getClientTLSCertificate", "structlibcdoc_1_1NetworkBackend.html#a70ad54354adf1db74e551d7833ec1142", null ],
+    [ "getLastErrorStr", "structlibcdoc_1_1NetworkBackend.html#ab855fdbee4a5e805cb09d34dca39da76", null ],
+    [ "getPeerTLSCertificates", "structlibcdoc_1_1NetworkBackend.html#a13ba51875b1cc1f2d439ae6970fa6c00", null ],
+    [ "getPeerTLSCertificates", "structlibcdoc_1_1NetworkBackend.html#a64b827e9f8bf8e836527c75a00f337d5", null ],
+    [ "operator=", "structlibcdoc_1_1NetworkBackend.html#a9c22ace9448c7c19a071d074cc9cc5b4", null ],
+    [ "sendKey", "structlibcdoc_1_1NetworkBackend.html#a59d8a5a2d635a0ecf9719acc847f1fbf", null ],
+    [ "sendShare", "structlibcdoc_1_1NetworkBackend.html#af986703b9bd2568ca6d87c694fa7b348", null ],
+    [ "showVerificationCode", "structlibcdoc_1_1NetworkBackend.html#a78d65f3295701fc44d37d72d5132bb25", null ],
+    [ "signMID", "structlibcdoc_1_1NetworkBackend.html#ac61fd0fb8656b2f804fd63e812a4989c", null ],
+    [ "signSID", "structlibcdoc_1_1NetworkBackend.html#a28579a528b9da1b350f850b0319edd85", null ],
+    [ "signTLS", "structlibcdoc_1_1NetworkBackend.html#a7f1bf148d93b154d70ab4c160631be98", null ],
+    [ "INVALID_PARAMS", "structlibcdoc_1_1NetworkBackend.html#a0808f7eb1ed6f857d18118a55b5db05f", null ],
+    [ "NETWORK_ERROR", "structlibcdoc_1_1NetworkBackend.html#ad2b1c912737aa996d487d6dd5c86af81", null ],
+    [ "NOT_IMPLEMENTED", "structlibcdoc_1_1NetworkBackend.html#a14f2e9b1f00f5d5447e7a78f4231192d", null ],
+    [ "OK", "structlibcdoc_1_1NetworkBackend.html#a12b8935f948e1eff920446c8a6e899c5", null ]
+];
