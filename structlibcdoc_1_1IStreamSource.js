5da2ab4bb1219e11cdca0ecf81698b8bc4966bf0
metsma <metsma@users.noreply.github.com>
2025-05-09 13:38:18 +0000

    deploy: 5410095532377031d9217934c1cecb85afaf471c

---
+++ structlibcdoc_1_1IStreamSource.js
@@ -0,0 +1,12 @@
+var structlibcdoc_1_1IStreamSource =
+[
+    [ "IStreamSource", "structlibcdoc_1_1IStreamSource.html#a1b1f47a830d6c9313249854de0f3b7d1", null ],
+    [ "IStreamSource", "structlibcdoc_1_1IStreamSource.html#a422f281e3b345c9bcbab3acc6e98f0d2", null ],
+    [ "~IStreamSource", "structlibcdoc_1_1IStreamSource.html#ac215141eb20862bcf54f0f0ff3f7c5f4", null ],
+    [ "isEof", "structlibcdoc_1_1IStreamSource.html#a895e74c866f2f35f9bbe2db0dd47b8a9", null ],
+    [ "isError", "structlibcdoc_1_1IStreamSource.html#aad17cdbdabc61a816951307a98fa3c44", null ],
+    [ "read", "structlibcdoc_1_1IStreamSource.html#adef37ace28ecd9843cdf31b69ec8b09d", null ],
+    [ "seek", "structlibcdoc_1_1IStreamSource.html#ab30a4c7e96e7fd107b5782d09f24a19f", null ],
+    [ "_ifs", "structlibcdoc_1_1IStreamSource.html#a0b6cd04e4e50c4fc9cb14f06763a3395", null ],
+    [ "_owned", "structlibcdoc_1_1IStreamSource.html#a271387181b8386fe6a3acf017abde6f5", null ]
+];
