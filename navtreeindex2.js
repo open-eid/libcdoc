22fc2dc73c87972eb1759db5b7828d56c40cd5f7
lauris71 <lauris71@users.noreply.github.com>
2025-06-12 08:03:00 +0000

    deploy: 8e6d7b77bcd09ff8e253e772645c84a297b91a24

---
+++ navtreeindex2.js
@@ -0,0 +1,15 @@
+var NAVTREEINDEX2 =
+{
+"structlibcdoc_1_1VectorSource.html#ad574c12937ef1ada7577bf7c80215f6c":[5,0,0,23,6],
+"structlibcdoc_1_1VectorSource.html#af6c96706d9dd4ce58ceba4b86230f434":[5,0,0,23,4],
+"structlibcdoc_1_1WinBackend.html":[5,0,0,24],
+"structlibcdoc_1_1WinBackend.html#a503bee8b2e1857dce074ba5cac1ec499":[5,0,0,24,8],
+"structlibcdoc_1_1WinBackend.html#a7bca2eca7cc49a032737adaea4a6358a":[5,0,0,24,1],
+"structlibcdoc_1_1WinBackend.html#a7dcc20c42199127df9dba34739b4b312":[5,0,0,24,0],
+"structlibcdoc_1_1WinBackend.html#a857f396e84d0eeefa2d1e470688ca572":[5,0,0,24,5],
+"structlibcdoc_1_1WinBackend.html#a931d6df8dfb3cf40281abfc93947a653":[5,0,0,24,2],
+"structlibcdoc_1_1WinBackend.html#a9f2145fb0361927a79478a7cb673afa6":[5,0,0,24,6],
+"structlibcdoc_1_1WinBackend.html#ad03c204104d3c7cd79ea2db759585823":[5,0,0,24,7],
+"structlibcdoc_1_1WinBackend.html#aef53beb43f769d97ee3c701561591efb":[5,0,0,24,3],
+"structlibcdoc_1_1WinBackend.html#af96034c7c77bb38dfe882609f8e998d8":[5,0,0,24,4]
+};
