f0b807adcc154449cf43565b58086e1c390a020c
lauris71 <lauris71@users.noreply.github.com>
2025-08-28 11:54:26 +0000

    deploy: 86b154a58be7284d19bc8a42e723a38604351d5e

---
+++ structlibcdoc_1_1FileListSource.js
@@ -0,0 +1,13 @@
+var structlibcdoc_1_1FileListSource =
+[
+    [ "FileListSource", "structlibcdoc_1_1FileListSource.html#a2b36772d16539f5d8c1800dd1b5f8ffa", null ],
+    [ "getNumComponents", "structlibcdoc_1_1FileListSource.html#a37b9acfc04a05c3acf77ceeb7995d6da", null ],
+    [ "isEof", "structlibcdoc_1_1FileListSource.html#a31b65e5b1343b640b0819c9f576407fd", null ],
+    [ "isError", "structlibcdoc_1_1FileListSource.html#ad3fe2f40a326f499a0da346eda711460", null ],
+    [ "next", "structlibcdoc_1_1FileListSource.html#a3029f2d74e5aae41d22637ed39bba20f", null ],
+    [ "read", "structlibcdoc_1_1FileListSource.html#a313367aa65eec1ab0270a7b260c6bf27", null ],
+    [ "_base", "structlibcdoc_1_1FileListSource.html#ad46c39dedb3886a121b22cf011d6a1ee", null ],
+    [ "_current", "structlibcdoc_1_1FileListSource.html#ad25b1eafbf8a64d39ec3db2f986e49b0", null ],
+    [ "_files", "structlibcdoc_1_1FileListSource.html#a34594e87aa0a3c6ebc6fd922d641959b", null ],
+    [ "_ifs", "structlibcdoc_1_1FileListSource.html#a74f40122d3f3f6844a3af4da15271c0c", null ]
+];
---
+++ structlibcdoc_1_1NetworkBackend_1_1ShareInfo.js
@@ -0,0 +1,5 @@
+var structlibcdoc_1_1NetworkBackend_1_1ShareInfo =
+[
+    [ "recipient", "structlibcdoc_1_1NetworkBackend_1_1ShareInfo.html#a2a4b66cf94c3a9204650f870cc57a4ce", null ],
+    [ "share", "structlibcdoc_1_1NetworkBackend_1_1ShareInfo.html#a107e59c814b4550babab889c0b3d9cbf", null ]
+];
