207ff5ef5b66aa673bc7ff1ae28f65db68daa657
lauris71 <lauris71@users.noreply.github.com>
2025-12-03 08:03:46 +0000

    deploy: 19d61d9533e797c824c1e8af1407505d88f8aae0

---
+++ navtreeindex2.js
@@ -0,0 +1,8 @@
+var NAVTREEINDEX2 =
+{
+"structlibcdoc_1_1WinBackend.html#a931d6df8dfb3cf40281abfc93947a653":[5,0,0,24,2],
+"structlibcdoc_1_1WinBackend.html#a9f2145fb0361927a79478a7cb673afa6":[5,0,0,24,6],
+"structlibcdoc_1_1WinBackend.html#ad03c204104d3c7cd79ea2db759585823":[5,0,0,24,7],
+"structlibcdoc_1_1WinBackend.html#aef53beb43f769d97ee3c701561591efb":[5,0,0,24,3],
+"structlibcdoc_1_1WinBackend.html#af96034c7c77bb38dfe882609f8e998d8":[5,0,0,24,4]
+};
---
+++ structlibcdoc_1_1VectorSource.js
@@ -0,0 +1,10 @@
+var structlibcdoc_1_1VectorSource =
+[
+    [ "VectorSource", "structlibcdoc_1_1VectorSource.html#a4d06185aa76eba920fd65c3ef97b074b", null ],
+    [ "isEof", "structlibcdoc_1_1VectorSource.html#a255f2b595b754063f3ba49f91f76dba9", null ],
+    [ "isError", "structlibcdoc_1_1VectorSource.html#a5753db43c20a8051e154be926d50e62a", null ],
+    [ "read", "structlibcdoc_1_1VectorSource.html#ab64d496d5ed80b6500679a6f3ffe1442", null ],
+    [ "seek", "structlibcdoc_1_1VectorSource.html#af6c96706d9dd4ce58ceba4b86230f434", null ],
+    [ "_data", "structlibcdoc_1_1VectorSource.html#aa8ca2dd7c63cb610a3303cd3916f6a91", null ],
+    [ "_ptr", "structlibcdoc_1_1VectorSource.html#ad574c12937ef1ada7577bf7c80215f6c", null ]
+];
