779591dd2497f95c2bc8ae50f7968a5196f09c5e
lauris71 <lauris71@users.noreply.github.com>
2026-02-13 20:35:23 +0000

    deploy: 351fd668383ca08dd6e24ab49f01d3453a747728

---
+++ structlibcdoc_1_1FileListSource.js
@@ -0,0 +1,13 @@
+var structlibcdoc_1_1FileListSource =
+[
+    [ "FileListSource", "structlibcdoc_1_1FileListSource.html#a2b36772d16539f5d8c1800dd1b5f8ffa", null ],
+    [ "getNumComponents", "structlibcdoc_1_1FileListSource.html#a0535a2211dfd2c414906256adef8a16e", null ],
+    [ "isEof", "structlibcdoc_1_1FileListSource.html#a7ea99debeb95d46e92d4aab8de19376f", null ],
+    [ "isError", "structlibcdoc_1_1FileListSource.html#af9295e952d2462cf16257f9305dcdcac", null ],
+    [ "next", "structlibcdoc_1_1FileListSource.html#a9bd6cbd0a9666166c7887a3e24405f25", null ],
+    [ "read", "structlibcdoc_1_1FileListSource.html#a1088f4405fde949a02e8fe70d48c3491", null ],
+    [ "_base", "structlibcdoc_1_1FileListSource.html#ad46c39dedb3886a121b22cf011d6a1ee", null ],
+    [ "_current", "structlibcdoc_1_1FileListSource.html#ad25b1eafbf8a64d39ec3db2f986e49b0", null ],
+    [ "_files", "structlibcdoc_1_1FileListSource.html#a34594e87aa0a3c6ebc6fd922d641959b", null ],
+    [ "_ifs", "structlibcdoc_1_1FileListSource.html#a74f40122d3f3f6844a3af4da15271c0c", null ]
+];
---
+++ structlibcdoc_1_1VectorSource.js
@@ -0,0 +1,10 @@
+var structlibcdoc_1_1VectorSource =
+[
+    [ "VectorSource", "structlibcdoc_1_1VectorSource.html#a4d06185aa76eba920fd65c3ef97b074b", null ],
+    [ "isEof", "structlibcdoc_1_1VectorSource.html#a5c4c3d7433327c57e7573ee1c5b4ca10", null ],
+    [ "isError", "structlibcdoc_1_1VectorSource.html#af56142ca27739bab7ebcc6a5744d0ff1", null ],
+    [ "read", "structlibcdoc_1_1VectorSource.html#abb0344451d7930d94a670114c7416a09", null ],
+    [ "seek", "structlibcdoc_1_1VectorSource.html#af6c96706d9dd4ce58ceba4b86230f434", null ],
+    [ "_data", "structlibcdoc_1_1VectorSource.html#aa8ca2dd7c63cb610a3303cd3916f6a91", null ],
+    [ "_ptr", "structlibcdoc_1_1VectorSource.html#ad574c12937ef1ada7577bf7c80215f6c", null ]
+];
