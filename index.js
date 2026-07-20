89092696e45e6c8469f6b504be40916d177f66f2
kristelmerilain <kristelmerilain@users.noreply.github.com>
2026-07-20 07:13:14 +0000

    deploy: 83408f9fcf4af25e81d891c34f9cc437d23f021a

---
+++ index.js
@@ -0,0 +1,7 @@
+var index =
+[
+    [ "Key Features", "index.html#autotoc_md1", null ],
+    [ "Use Cases", "index.html#autotoc_md2", null ],
+    [ "Licensing and Contributions", "index.html#autotoc_md3", null ],
+    [ "Library Architecture", "index.html#autotoc_md4", null ]
+];
---
+++ structlibcdoc_1_1FileInfo.js
@@ -0,0 +1,5 @@
+var structlibcdoc_1_1FileInfo =
+[
+    [ "name", "structlibcdoc_1_1FileInfo.html#afdf97fb1a21cd74ce75610be94eab6ac", null ],
+    [ "size", "structlibcdoc_1_1FileInfo.html#a918897af66d303965bc9d9a094293bea", null ]
+];
